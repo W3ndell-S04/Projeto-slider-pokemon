b232345363b9cbbbda6e933f7840e716e5dec4b4
W3ndell-S04 <106290138+W3ndell-S04@users.noreply.github.com>
2022-07-31 13:50:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+/* variaveis */
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+/* avancar */
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao (indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado");
+}
+
+btnAvancar.addEventListener("click", function () {
+  if(cartaoAtual === cartoes.length -1) return
+
+  esconderCartaoSelecionado();
+  
+  
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual)
+  
+});
+
+/* voltar */
+
+btnVoltar.addEventListener("click", function () {
+  
+  if(cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+  
+  
+ 
+});
